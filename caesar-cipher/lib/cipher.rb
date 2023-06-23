da69bec8b40847a11ebaf0eecbde83118c0e0bd3
Elton <28992495+Lucrum@users.noreply.github.com>
2023-06-23 18:27:59 -0400

    Fixed cipher for all test cases

--- caesar-cipher/lib/cipher.rb
+++ caesar-cipher/lib/cipher.rb
@@ -7,11 +7,20 @@ def caesar_cipher(string, shift)
 end
 
 def shift_char(char, shift)
-    if char.between?(65, 90) || char.between?(97, 122)
-        char += shift
-        unless char.between?(65, 90) || char.between?(97, 122)
+    bounds = char.between?(65, 90) ? 1 :
+        char.between?(97, 122) ? 2 : nil
+    if bounds
+        char += shift % 26
+    case bounds
+    when 1
+        unless char.between?(65, 90)
             char -= 26
         end
+    when 2
+        unless char.between?(97, 122)
+            char -= 26
+        end
+    end
     end
     char.chr
 end
