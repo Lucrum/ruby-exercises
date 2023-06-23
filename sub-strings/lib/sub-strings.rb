2106d5fd0ec4ce91873533db929312d215a2da51
Elton <28992495+Lucrum@users.noreply.github.com>
2023-06-23 19:13:36 -0400

    Applied fix for second test

--- sub-strings/lib/sub-strings.rb
+++ sub-strings/lib/sub-strings.rb
@@ -2,8 +2,10 @@ def substrings(word, dictionary)
     res = Hash.new(0)
 
     dictionary.each do |dict_word|
-        if word.include? dict_word
+        temp = word.downcase
+        while temp.include? dict_word
             res[dict_word] += 1
+            temp = temp.sub(dict_word, '')
         end
     end
     res
