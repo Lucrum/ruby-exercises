0bf51a0baa1c2c644a33efeceea497f987b5ef81
Elton <28992495+Lucrum@users.noreply.github.com>
2023-06-27 16:55:32 -0400

    Added tests

--- bubble-sort/spec/bubble_sort_spec.rb
+++ bubble-sort/spec/bubble_sort_spec.rb
@@ -1,1 +1,16 @@
 require './lib/bubble.rb'
+
+describe 'Bubble sort' do
+    it 'sorts a list' do
+        arr = [4,3,78,2,0,2]
+        expect(bubble_sort(arr)).to eql(arr.sort)
+    end
+
+    it 'returns an empty array if array is empty' do
+        expect(bubble_sort([])).to eql([])
+    end
+
+    it 'returns the same array if the array has one element' do
+        expect(bubble_sort[2]).to eql([2])
+    end
+end
