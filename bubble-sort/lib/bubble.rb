9b688eff7d094c1a69faf1053eb73cf30d42acaf
Elton <28992495+Lucrum@users.noreply.github.com>
2023-06-27 18:36:01 -0400

    Update to match Ruby styling guidelines

--- bubble-sort/lib/bubble.rb
+++ bubble-sort/lib/bubble.rb
@@ -1,19 +1,15 @@
 def bubble_sort(arr)
-    no_swaps = false
-
-    tmp_arr = arr.dup
-
-    until no_swaps
-        no_swaps = true
-        tmp_arr.each_with_index do |num, index|
-            if index < tmp_arr.length - 1 && num > tmp_arr[index + 1]
-                tmp_arr[index] = tmp_arr[index + 1]
-                tmp_arr[index + 1] = num
-                no_swaps = false
-            end
-        end
+  no_swaps = false
+  tmp_arr = arr.dup
+  until no_swaps
+    no_swaps = true
+    tmp_arr.each_with_index do |num, index|
+      if index < tmp_arr.length - 1 && num > tmp_arr[index + 1]
+        tmp_arr[index] = tmp_arr[index + 1]
+        tmp_arr[index + 1] = num
+        no_swaps = false
+      end
     end
-
-    tmp_arr
-
+  end
+  tmp_arr
 end
--- bubble-sort/spec/bubble_sort_spec.rb
+++ bubble-sort/spec/bubble_sort_spec.rb
@@ -1,17 +1,16 @@
-require './lib/bubble.rb'
+require './lib/bubble'
 
 describe 'Bubble sort' do
-    it 'sorts a list' do
-        arr = [4,3,78,2,0,2]
-        ans = [0,2,2,3,4,78]
-        expect(bubble_sort(arr)).to eql(arr.sort)
-    end
+  it 'sorts a list' do
+    arr = [4, 3, 78, 2, 0, 2]
+    expect(bubble_sort(arr)).to eql(arr.sort)
+  end
 
-    it 'returns an empty array if array is empty' do
-        expect(bubble_sort([])).to eql([])
-    end
+  it 'returns an empty array if array is empty' do
+    expect(bubble_sort([])).to eql([])
+  end
 
-    it 'returns the same array if the array has one element' do
-        expect(bubble_sort([2])).to eql([2])
-    end
+  it 'returns the same array if the array has one element' do
+    expect(bubble_sort([2])).to eql([2])
+  end
 end
